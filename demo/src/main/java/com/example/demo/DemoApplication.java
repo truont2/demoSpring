5f08bc5825f31c5195f0ab4e21a7a8dc0fedea02
Takara Truong <truont2@gmail.com>
2022-08-25 13:36:43 -0700

    created connection to postgresql database

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
+import net.bytebuddy.asm.Advice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+//barebone spring application
 @SpringBootApplication
+//makes this class serve rest endpoints
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
